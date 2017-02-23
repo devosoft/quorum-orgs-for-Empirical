d339809d6f557711a68f36f28d29b3ce7e3c1498
Jake Fenton <bocajnotnef@gmail.com>
2017-02-23 14:35:49 -0500

    dunno what this is, but it's there

---
+++ module/QuorumPopManager.h
@@ -0,0 +1,21 @@
+//  This file is part of Empirical, https://github.com/devosoft/Empirical
+//  Copyright (C) Michigan State University, 2017.
+//  Released under the MIT Software license; see doc/LICENSE
+
+/** This file contains the quorum-organism population manager adapter, featuring goodies such as
+ * calculating what organisms are at quorum and get points.
+ */
+
+#pragma once
+
+namespace emp {
+namespace evo {
+
+template <typename BASE_POP>
+class QuourmPopManager : public BASE_POP {
+
+
+}; // end QPM class
+
+} // namespace evo
+} // namespace emp
