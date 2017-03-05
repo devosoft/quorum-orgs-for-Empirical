1b39f84a548cc2db9e0feec20929110039c3bd21
Anya Johnson <anyaejo@gmail.com>
2017-03-05 14:09:49 -0500

    fixed org placement in grid and pools

--- QSPop.h
+++ QSPop.h
@@ -36,7 +36,7 @@ namespace emp{
 
       int AddOrgBirth(ORG * new_org, int parent_pos) {
 	emp::vector<int> empty_spots = GetEmptyPopIDs();
-	if(empty_spots.size()){
+	if(empty_spots.size()>0){
 	  const int pos = empty_spots[random_ptr->GetInt(empty_spots.size())];
 	  emp_assert(!pop[pos], "Empty cell not actually empty", pos);
 	  pop[pos] = new_org;
@@ -60,8 +60,8 @@ namespace emp{
       using PopulationManager_Grid<ORG>::ToY;
       using PopulationManager_Grid<ORG>::ToID;
 
-      int width;
-      int height;
+      using PopulationManager_Grid<ORG>::width;
+      using PopulationManager_Grid<ORG>::height;
 
       std::set<int> GetNeighborIDs (int org_id){
 	std::set<int> neighbor_ids;
@@ -87,7 +87,7 @@ namespace emp{
 	  if(!pop[n]) empty_neighbors.push_back(n);
 	}
 	
-	if(empty_neighbors.size()){
+	if(empty_neighbors.size()>0){
 	  int pos = empty_neighbors[random_ptr->GetInt((int) empty_neighbors.size())];
 	  emp_assert(!pop[pos], "Empty cell not empty!", pos);
 	  pop[pos] = new_org;
@@ -107,28 +107,34 @@ namespace emp{
       using PopulationManager_Base<ORG>::random_ptr;
       using PopulationManager_Base<ORG>::GetSize;
 
-      vector<int> pool_end;
-      std::map<int, vector<int>> connections;
-      double mig_rate;
-      vector<int> pool_id;
+      using PopulationManager_GridPools<ORG>::ToX;
+      using PopulationManager_GridPools<ORG>::ToY;
+      using PopulationManager_GridPools<ORG>::ToID;
+      using PopulationManager_GridPools<ORG>::pool_end;
+
+
+
 
       int AddOrgBirth(ORG * new_org, int parent_pos) {
+	//Even though we're inheriting from Grid Pools, we aren't going to bother with neighborhood reproduction inside pool since we are trying to simulate 96-well plates which are mixed within the plate
 	int InsertPool = 0;
-	
-	const auto & parent_conns = connections[parent_pos];
-	if (random_ptr->P(mig_rate) && parent_conns.size() > 0) {
-	  int conn_id = random_ptr->GetInt(0, parent_conns.size());
-	  InsertPool = parent_conns[conn_id];
-        }
-        else{ InsertPool = pool_id[parent_pos]; }
-
-	int range_l = InsertPool ? pool_end[InsertPool-1] : 0;
-	int range_u = pool_end[InsertPool];
+	int range_l, range_u;
 	emp::vector<int> empty;
+
+	for(int i=0; i<pool_end.size(); i++){
+	  if(parent_pos < pool_end[i]){InsertPool = i; break;}
+	}
+
+	if(InsertPool==0){range_l=0;}
+	else{range_l = pool_end[InsertPool-1];}
+	range_u = pool_end[InsertPool];
+
 	for(int i=range_l; i<range_u; i++){
 	  if(!pop[i]) empty.push_back(i);
 	}
-	if(empty.size()){
+
+	
+	if(empty.size()>0){
 	  const int pos = empty[random_ptr->GetInt(empty.size())];
 	  emp_assert(!pop[pos], "Empty cell not empty!", pos);
 	  pop[pos] = new_org;
